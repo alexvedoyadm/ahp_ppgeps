c46353293aed2d5c85b06681de28ecf9fe101253
alexvedoyadm <111555178+alexvedoyadm@users.noreply.github.com>
2023-09-30 20:00:37 -0300

    Add files via upload

--- ahp_ppgeps.py
+++ ahp_ppgeps.py
@@ -8,7 +8,7 @@ def calcular_matriz_comparacao(df, num_critérios):
     for index, row in df.iterrows():
         crit1, crit2 = map(int, row["Critérios/Alternativas"].split())
         importancia, valor = row["Importância/Valor"]
-        
+
         if importancia == "+":
             matriz_comparacao[crit1 - 1, crit2 - 1] = valor
             matriz_comparacao[crit2 - 1, crit1 - 1] = 1 / valor
@@ -65,6 +65,29 @@ def calcular_rc_max(tamanho_matriz):
     }
     return tabela_rc_max.get(tamanho_matriz, 0.10)
 
+def criar_tabela(matriz, soma_colunas=None):
+    num_critérios = matriz.shape[0]
+    tabela = pd.DataFrame(
+        matriz,
+        columns=[f"C{i+1}" for i in range(num_critérios)],
+        index=[f"C{i+1}" for i in range(num_critérios)],
+    )
+
+    if soma_colunas is not None:
+        tabela.loc["Soma das Colunas"] = soma_colunas
+
+    return tabela
+
+def calcular_media_pesos(matriz_pesos):
+    num_critérios = matriz_pesos.shape[0]
+    media_pesos = matriz_pesos.mean(axis=1)
+    tabela_media_pesos = pd.DataFrame(
+        media_pesos,
+        columns=["Média de Pesos"],
+        index=[f"C{i+1}" for i in range(num_critérios)],
+    )
+    return tabela_media_pesos
+
 def pagina_numero_critérios():
     st.title("Para quantos critérios você quer atribuir pesos?")
     num_critérios = st.number_input("Você deve utilizar entre 3 e 7 critérios", min_value=3, max_value=7, step=1)
@@ -89,7 +112,19 @@ def pagina_importancia_valor():
     for i in range(1, num_critérios + 1):
         for j in range(i + 1, num_critérios + 1):
             importancia = st.selectbox(f" C{i} é mais importante, menos importante, ou tem a mesma importância que C{j} ?", ["+", "-", "="])
-            valor = st.number_input(f"De 0 (zero) a 9 (nove), quão importante é esse critério? C{i} C{j}", min_value=1, max_value=9, step=1)
+            valor = st.slider(f"Qual a relação de importância entre os critérios C{i} C{j}?", 1, 9, 5)
+            descricao_slider = {
+                1: "Importância igual (Contribuição idêntica das alternativas ou critérios)",
+                2: "Importância entre Igual e Fraca",
+                3: "Importância Fraca (Julgamento levemente superior para uma alternativa ou critério)",
+                4: "Importância entre Fraca e Forte",
+                5: "Importância Forte (Julgamento fortemente a favor de uma alternativa ou critério)",
+                6: "Importância entre Forte e Muito Forte",
+                7: "Importância Muito Forte (Dominância reconhecida de uma alternativa ou critério)",
+                8: "Importância entre Muito Forte e Absoluta",
+                9: "Importância absoluta (Dominância comprovada de uma alternativa ou critério)"
+            }
+            st.write(descricao_slider[valor])
             data["Critérios/Alternativas"].append(f"{i} {j}")
             data["Importância/Valor"].append((importancia, valor))
 
@@ -103,12 +138,13 @@ def pagina_importancia_valor():
         tabela_comparacao = criar_tabela(matriz_comparacao, soma_colunas)
         tabela_pesos = criar_tabela(matriz_pesos)
 
-        st.title("Matrizes de Comparação e Pesos")
+        st.write("___")
         st.subheader("Matriz de Comparação:")
         st.dataframe(tabela_comparacao)
+        st.write("___")
         st.subheader("Matriz de Pesos:")
         st.dataframe(tabela_pesos)
-
+        st.write("___")
         st.subheader("Pesos:")
         tabela_media_pesos = calcular_media_pesos(matriz_pesos)
         st.dataframe(tabela_media_pesos)
@@ -121,41 +157,34 @@ def pagina_importancia_valor():
         tamanho_matriz = f"{num_critérios} x {num_critérios}"
         rc_max = calcular_rc_max(tamanho_matriz)
 
+        st.write("___")
         st.title("Avaliação de Consistência")
         avaliacao_consistencia = {
             "Avaliação Consistência": ["MH(s)", "N", "ICH", "HRI", "RC", "RCMáx"],
             "Valor": [mh_s, num_critérios, ich, hri, rc, rc_max],
+
         }
 
-        st.table(pd.DataFrame(avaliacao_consistencia).T)
+        # Estilização da tabela
+        st.markdown('<style>table {border-collapse: collapse;} th, td {border: 1px solid black; padding: 8px; text-align: center;} th {background-color: #f2f2f2;}</style>', unsafe_allow_html=True)
 
-def criar_tabela(matriz, soma_colunas=None):
-    num_critérios = matriz.shape[0]
-    tabela = pd.DataFrame(
-        matriz,
-        columns=[f"C{i+1}" for i in range(num_critérios)],
-        index=[f"C{i+1}" for i in range(num_critérios)],
-    )
+        # Ocultando a primeira linha
+        st.table(pd.DataFrame(avaliacao_consistencia).set_index('Avaliação Consistência'))
 
-    if soma_colunas is not None:
-        tabela.loc["Soma das Colunas"] = soma_colunas
+# Configuração da página Streamlit
+st.set_page_config(
+    page_title="AHP PPGEPS",
+    page_icon="✅",
+    layout="wide",
+)
 
-    return tabela
+# Execução do aplicativo
+if __name__ == "__main__":
+    st.title("Analytic Hierarchy Process (AHP)")
+    if "pagina_atual" not in st.session_state:
+        st.session_state.pagina_atual = "numero_critérios"
 
-def calcular_media_pesos(matriz_pesos):
-    num_critérios = matriz_pesos.shape[0]
-    media_pesos = matriz_pesos.mean(axis=1)
-    tabela_media_pesos = pd.DataFrame(
-        media_pesos,
-        columns=["Média de Pesos"],
-        index=[f"C{i+1}" for i in range(num_critérios)],
-    )
-    return tabela_media_pesos
-
-if "pagina_atual" not in st.session_state:
-    st.session_state.pagina_atual = "numero_critérios"
-
-if st.session_state.pagina_atual == "numero_critérios":
-    pagina_numero_critérios()
-elif st.session_state.pagina_atual == "importancia_valor":
-    pagina_importancia_valor()
+    if st.session_state.pagina_atual == "numero_critérios":
+        pagina_numero_critérios()
+    elif st.session_state.pagina_atual == "importancia_valor":
+        pagina_importancia_valor()
